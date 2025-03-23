5606c9f508c0f759ba194efd7c6363785ddd57a0
Omodamola Oladeji <darmhoo@hotmail.com>
2025-03-23 19:54:56 +0000

    done

--- src/modules/admin/pages/Student.tsx
+++ src/modules/admin/pages/Student.tsx
@@ -2,112 +2,23 @@ import Footer from '@app/modules/main/footer/Footer';
 import { ContentHeader } from '@components';
 import DataTable from '../../../components/datatable-original/Datatable';
 
-import { toast } from 'react-toastify';
-import axios from '../../../utils/axios';
-import React, { useEffect, useMemo } from 'react';
 
-import { fetchAllStudents, createStudent, updateStudentStatus, deleteStudent, deactivateStudent, fetchStudentById } from '@app/services/admin/studentServices';
-import FilterComponent from '@app/components/data-table/FilterComponent';
+import React, { useEffect } from 'react';
+
+import { fetchStudentById } from '@app/services/admin/studentServices';
 import { ColorRing } from 'react-loader-spinner';
-import { Button, Card, Col, Container, ListGroup, Modal, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
-import { useNavigate, useParams } from 'react-router-dom';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Card, Col, Container, ListGroup, Row, } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+
 
 
 
 const Student = () => {
     const { id } = useParams();
 
-    const [open, setOpen] = React.useState(false);
     const [student, setStudent] = React.useState<any>();
-    const [pending, setpending] = React.useState(true);
     const [loading, setLoading] = React.useState(false);
-    const [openAdd, setOpenAdd] = React.useState(false);
-    const [openEdit, setOpenEdit] = React.useState(false);
-    const [openDelete, setOpenDelete] = React.useState(false);
-    const [editStudentStatus, setEditStudentStatus] = React.useState(false);
-    const [editStudentSub, setEditStudentSub] = React.useState(false);
-    const [filterText, setFilterText] = React.useState("");
-    const [resetPaginationToggle, setResetPaginationToggle] = React.useState(
-        false
-    );
-    const [selectedStudent, setSelectedStudent] = React.useState<any>();
-    const [filename, setFilename] = React.useState("");
-    const [file, setFile] = React.useState(null);
-    const [email, setEmail] = React.useState('');
-    const [firstName, setFirstName] = React.useState('');
-    const [lastName, setLastName] = React.useState('');
-    const [regNo, setRegNo] = React.useState('');
-    const [phoneNumber, setPhoneNumber] = React.useState('');
 
-    const [rows, setRows] = React.useState([]);
-    const navigate = useNavigate();
-
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const handleOpenEdit = () => {
-        setOpenEdit(true);
-    };
-    const handleCloseEdit = () => {
-        setOpenEdit(false);
-    };
-
-
-
-    const handleOpenDelete = () => {
-        setOpenDelete(true);
-    };
-    const handleCloseDelete = () => {
-        setOpenDelete(false);
-    };
-
-    const handleButtonClick = (type: any, student: any) => {
-        setSelectedStudent(student);
-        if (type === 'edit') {
-            // await changeStudentPass(student.id);
-            // toast.success('Student updated Successfully!');
-
-            setEditStudentStatus(student.is_admin);
-            setEditStudentSub(student.profile.subscription === 'premium')
-            handleOpenEdit();
-        } else if (type === 'delete') {
-            setSelectedStudent(student);
-            handleOpenDelete();
-        }
-    }
-
-
-
-
-
-
-
-
-
-    const downloadStudents = async () => {
-        setLoading(true)
-        await axios.get('/admin/students-export', { responseType: 'blob' }).then((res: any) => {
-            const url = window.URL.createObjectURL(new Blob([res.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'Students.xlsx'); //or any other extension
-            document.body.appendChild(link);
-            link.click();
-            toast.success("Request was successful");
-
-        }).finally(() => {
-            setLoading(false);
-        })
-
-    }
 
     const getStudent = async () => {
         try {
