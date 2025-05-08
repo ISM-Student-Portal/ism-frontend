3e68db21001839b844e5fb0722e4b6ce9181c551
Omodamola Oladeji <darmhoo@hotmail.com>
2025-05-08 10:10:31 +0000

    added files

--- src/pages/student_pages/StudentClassroom.tsx
+++ src/pages/student_pages/StudentClassroom.tsx
@@ -85,7 +85,8 @@ const StudentClassroom = () => {
 
         { name: 'Title', selector: (row: any) => row.title },
         { name: 'Description', selector: (row: any) => row.description },
-        { name: 'Link', selector: (row: any) => (<a target='_blank' href={row.link}>{row.link}</a>) },
+        { name: 'Link', selector: (row: any) => (<a target='_blank' href={row.link} rel='noreferrer'>{row.link}</a>) },
+        { name: 'File', selector: (row: any) => (<a target='_blank' href={row.resource} rel='noreferrer'>{row.resource}</a>) },
         { name: 'Attendance', selector: (row: any) => row.attendance ? (<span className='text-success'>Attended</span>) : row.attendance === null && moment() < moment(row.expires_on) ? (<span></span>) : (<span className='text-danger'>Missed</span>), },
         { name: 'Expiry', selector: (row: any) => row.expires_on, sortable: true },
 
