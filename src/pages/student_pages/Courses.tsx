50574612b14d4277b2ccf9770f7f676e8bc0c397
Omodamola Oladeji <darmhoo@hotmail.com>
2025-05-14 15:24:16 +0000

    cleaned courses

--- src/pages/student_pages/Courses.tsx
+++ src/pages/student_pages/Courses.tsx
@@ -1,33 +1,24 @@
 import Footer from '@app/modules/main/footer/Footer';
 import { ContentHeader } from '@components';
-import DataTable from '../../components/datatable-original/Datatable';
 import React, { useEffect } from 'react';
 
-import VisibilityIcon from '@mui/icons-material/Visibility';
 
-import { toast } from 'react-toastify';
 
-import { createStudent, updateStudentStatus, deleteStudent } from '@app/services/admin/studentServices';
 import { ColorRing } from 'react-loader-spinner';
 import { fetchAllCourses } from '@app/services/admin/courseServices';
-import { useNavigate } from 'react-router-dom';
-import { OverlayTrigger, Tooltip, Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 
 const Courses = () => {
 
-    const [pending, setPending] = React.useState<any>();
     const [loading, setLoading] = React.useState<any>(false);
     const [rows, setRows] = React.useState([]);
-    const navigate = useNavigate();
-
 
     const getCourses = async () => {
         try {
             setLoading(true);
             const courses = await fetchAllCourses();
             setRows(courses.courses);
-            setPending(false);
         } catch (error) {
 
         }
@@ -36,31 +27,6 @@ const Courses = () => {
         }
 
     }
-
-
-
-
-
-
-
-
-    const style = {
-        position: 'absolute' as 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 600,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        pt: 2,
-        px: 4,
-        pb: 3,
-    };
-
-
-
-
     useEffect(() => {
         getCourses();
     }, [])
@@ -79,7 +45,7 @@ const Courses = () => {
 
                             <div className='row gap-2'>
                                 {rows.length > 0 && rows.map((item: any) => (
-                                    <Card className='col-2 mx-2'>
+                                    <Card className='col-lg-2 mx-2 align-items-center'>
                                         <Card.Img variant="top" src={item?.lecturer.profile_pix_url ? item?.lecturer.profile_pix_url : "/img/default-profile.png"} />
                                         <Card.Body>
                                             <Card.Title><b>Title: </b>{item.title}</Card.Title>
